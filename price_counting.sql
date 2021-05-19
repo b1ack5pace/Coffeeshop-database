a25177607c36b1be2cb00e47826966fe57deb6e1
b1ack5pace <47659629+b1ack5pace@users.noreply.github.com>
2021-05-19 18:34:18 +0300

    Create price_counting.sql

---
+++ price_counting.sql
@@ -0,0 +1,10 @@
+This function calculates the price of an order, including personal discount of the exact client
+
+CREATE OR REPLACE FUNCTION public.price_counting()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN UPDATE orders set total_price = (product_amount * (SELECT price FROM products WHERE NEW.product_id = products.product_id) *
+    (1 - ((SELECT discount FROM clients WHERE NEW.client_id = clients.client_id) *0.01)));
+RETURN NULL; END;
+$function$
