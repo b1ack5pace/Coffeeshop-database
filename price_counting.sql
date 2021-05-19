2552a0a61ad3ddcec5e21369b895e21b26b97917
b1ack5pace <47659629+b1ack5pace@users.noreply.github.com>
2021-05-19 20:41:45 +0300

    Update price_counting.sql

--- price_counting.sql
+++ price_counting.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE FUNCTION public.price_counting()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
-BEGIN UPDATE orders set total_price = (product_amount * (SELECT price FROM products WHERE NEW.product_id = products.product_id) *
-    (1 - ((SELECT discount FROM clients WHERE NEW.client_id = clients.client_id) *0.01)));
-RETURN NULL; END;
-$function$
+BEGIN UPDATE orders SET total_price = (product_amount  * (SELECT price FROM products WHERE NEW.product_id = products.product_id) *
+                                       (1 - ((SELECT discount FROM clients WHERE NEW.client_id = clients.client_id) *0.01))) WHERE NEW.order_id = orders.order_id;
+RETURN NEW; END;
+$function$;
